72118bac8435179478f76ced7f39443f8c7101a3
Javier Gonzalez <javig56@gmail.com>
2018-05-03 13:09:58 -0700

    chatroom needs more styling per user name

--- GroupChat/server.js
+++ GroupChat/server.js
@@ -16,16 +16,17 @@ app.get("/", function(req, res){
 var messages = [];
 var count = 0;
 io.on('connection', function(socket){
-    socket.emit("greeting", {msg: "Welcome to the Coversation Board"}, messages, count);
+    socket.emit("greeting", {msg: "Welcome to the Coversation Board"}, messages, count, socket.id);
     count++;
     if(count == 10){
         count = 0;
     }
+    console.log(socket.id)
     socket.on("newmessage", function(message, user){
         console.log("message",message);
         console.log("user", user);
         messages.push(`${user}: ${message}`);
         console.log(messages);
-        io.emit("clientmessage", message, user);
+        io.emit("clientmessage", message, user, socket.id);
     });
 })
